0de37f296b8f6b212a6a44ccb02025250c693056
Godswilly <kalu.agukalu@gmail.com>
2022-06-11 21:54:01 +0100

    Add chat room page

---
+++ client/src/Components/chatRoom.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import useChat from '../useChat';
+
+const ChatRoom = (props) => {
+	const { roomId } = props.match.params;
+	const { messages, sendMessage } = useChat(roomId);
+	const [newMessage, setNewMessage] = useState('');
+
+	const handleNewMessageChange = (event) => {
+		setNewMessage(event.target.value);
+	};
+
+	const handleSendMessage = () => {
+		sendMessage(newMessage);
+		setNewMessage('');
+	};
+
+	return (
+		<div className='chat-room'>
+			<h1 className='room-name'>Room: {roomId}</h1>
+			<div className='messages'>
+				<ol className='message-list'>
+					{messages.map((message, i) => (
+						<li
+							key={i}
+							className={`message-item ${
+								message.ownedByCurrentUser ? 'my-message' : 'received-message'
+							}`}
+						>
+							{message.body}
+						</li>
+					))}
+				</ol>
+			</div>
+			<textarea
+				value={newMessage}
+				onChange={handleNewMessageChange}
+				placeholder='Write message...'
+				className='new-message-input-field'
+			/>
+			<button onClick={handleSendMessage} className='send-message-button'>
+				Send
+			</button>
+		</div>
+	);
+};
+
+export default ChatRoom;
